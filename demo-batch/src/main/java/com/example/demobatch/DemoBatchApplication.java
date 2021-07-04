847c2c23a75a0ee5c17c7d4cb8cdd9e67d62400f
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-07-04 17:53:10 -0300

    First commit

---
+++ demo-batch/src/main/java/com/example/demobatch/DemoBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBatchApplication.class, args);
+	}
+
+}
